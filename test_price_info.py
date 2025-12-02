2838fe84e35132e9664926d759c19b01f2187319
JH_Lab1 <jiaheng.24@ichat.sp.edu.sg>
2025-12-02 10:35:23 +0800

    cleaned up code for test cases

--- test_price_info.py
+++ test_price_info.py
@@ -3,17 +3,16 @@ import price_info
 print("Test price_info")
 
 def test_total_cost_shopping():
-    result=0
-    test = 46.75
+    expected_result = 46.75
     
     result = price_info.total_cost_shopping()
     
-    assert(result==test)
+    assert(result==expected_result)
 
 def test_cost_of_fruits():
-    result=0
-    test = 12.6
+    
+    expected_result = 12.6
 
     result = price_info.cost_of_fruits("orange",9)
 
-    assert(result==test)
+    assert(result==expected_result)
